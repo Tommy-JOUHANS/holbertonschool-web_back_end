8741d541ff85a84307334a94e41884d745ca2195
Tommy JOUHANS <12119@holbertonstudents.com>
2026-04-06 18:28:15 +0200

    to str

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Module that contains a type-annotated function to_str that takes
+a float n as argument and returns the string representation of the float."""
+
+
+def to_str(n: float) -> str:
+    """Return the string representation of n as a str."""
+    return str(n)
