8c8b2a4c66544f4fe9490ab7ec12b781321de832
Tommy JOUHANS <12119@holbertonstudents.com>
2026-04-06 18:05:59 +0200

    concat

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Module that contains a type-annotated function concat that takes two strings
+and returns their concatenation as a string.
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Return the concatenation of str1 and str2 as a string."""
+    return str1 + str2
