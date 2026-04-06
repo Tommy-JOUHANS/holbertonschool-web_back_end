926759a40af890d04dc861eb281275513744d08c
Tommy JOUHANS <12119@holbertonstudents.com>
2026-04-06 18:42:38 +0200

    4-define_variables.py

--- python_variable_annotations/4-define_variables.py
+++ python_variable_annotations/4-define_variables.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
-"""Module that contains a definition and annotation of the following variables with 
-the specified values"""
+"""Module that contains a definition and annotation
+of the following variables with the specified values"""
 
 
 a: int = 1
-pi: float = 3.14        
+pi: float = 3.14
 i_understand_annotations: bool = True
 school: str = "Holberton"
