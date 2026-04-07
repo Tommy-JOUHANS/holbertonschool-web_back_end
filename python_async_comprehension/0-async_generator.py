6402266bc1192ece22e0f05147c123e3626979d1
Tommy JOUHANS <12119@holbertonstudents.com>
2026-04-07 15:55:30 +0200

    0-async_generator.py

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -15,5 +15,5 @@ async def async_generator():
 
     for _ in range(10):
         delay = random.uniform(0, 10)
-        await asyncio.sleep(1)
+        await asyncio.sleep(1, result=delay)
         yield delay
