e714c4ec0964d99f80e6b9741440dad5e55e46f9
Tommy JOUHANS <12119@holbertonstudents.com>
2026-04-07 15:29:47 +0200

    0-async_generator.py

---
+++ python_async_comprehension/0-async_generator.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""Module that contains a coroutine called async_generator
+that takes no arguments."""
+
+
+import asyncio
+import random
+
+
+async def async_generator() -> float:
+    """Coroutine called async_generator that takes no arguments.
+    The coroutine will loop 10 times, each time asynchronously
+    wait for a random delay between 0 and 10 seconds, then yield
+    the delay."""
+
+    for _ in range(10):
+        delay = random.uniform(0, 10)
+        await asyncio.sleep(delay)
+        yield delay
