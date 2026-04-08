f468da9596d7fd179e907ff3f50a8e6b2fc304d7
Tommy JOUHANS <12119@holbertonstudents.com>
2026-04-08 12:08:41 +0200

    2-measure_runtime.py

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -3,9 +3,10 @@
 
 import asyncio
 async_comprehension = __import__('1-async_comprehension').async_comprehension
+from typing import List
 
 
-async def measure_runtime():
+async def measure_runtime() -> List[float]:
     """Coroutine called measure_runtime that takes no arguments.
     The coroutine will execute async_comprehension four times in
     parallel using asyncio.gather and return the total runtime."""
