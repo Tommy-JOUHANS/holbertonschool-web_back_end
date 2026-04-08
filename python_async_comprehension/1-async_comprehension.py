b105c52b0c4054e5366bd1c0e1e1248dfb80abde
Tommy JOUHANS <12119@holbertonstudents.com>
2026-04-08 12:06:03 +0200

    1-async_comprehension.py

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -2,10 +2,12 @@
 """Module that contains a coroutine called
 async_comprehension that takes no arguments."""
 
+
+from typing import List
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension():
+async def async_comprehension() -> List[float]:
     """Coroutine called async_comprehension that takes no arguments.
     The coroutine will collect 10 random delays using an async
     comprehensing over async_generator, then return the 10 delays."""
