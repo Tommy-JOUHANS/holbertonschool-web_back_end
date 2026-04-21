726255c34d89254ebd998ea7c4748c35e13a91dd
Tommy JOUHANS <12119@holbertonstudents.com>
2026-04-21 10:17:29 +0200

    10-update_topics.py

---
+++ NoSQL/10-update_topics.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Python function that changes all topics of a school
+document based on the name
+"""
+
+
+def update_topics(mongo_collection, name, topics):
+    """Changes all topics of a school document based on the name
+    Args:
+        mongo_collection: The collection to update the document in
+        name: The name of the school to update
+        topics: The list of topics to set for the school
+    Returns:
+        The number of documents updated
+    """
+    result = mongo_collection.update_many(
+        {'name': name},
+        {'$addToSet': {'topics': {'$each': topics}}}
+    )
+    return result.modified_count
