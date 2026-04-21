1b46d07b56c72b96e932dcd34787af58f6530d47
Tommy JOUHANS <12119@holbertonstudents.com>
2026-04-21 14:17:47 +0200

    12-log_stats.py

---
+++ NoSQL/12-log_stats.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Python script that provides some stats about Nginx logs stored in MongoDB
+"""
+from pymongo import MongoClient
+
+if __name__ == "__main__":
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    nginx = client.logs.logs
+    print("{} logs".format(nginx.count_documents({})))
+    print("Methods:")
+    for method in ["GET", "POST", "PUT", "PATCH", "DELETE"]:
+        print("\tmethod {}: {}".format(method, nginx.count_documents(
+            {"method": method})))
+    print("{} status check".format(nginx.count_documents(
+        {"method": "GET", "path": "/status"})))
