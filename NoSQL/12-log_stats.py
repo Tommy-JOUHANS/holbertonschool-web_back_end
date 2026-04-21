92334160eecbee193d569ed95afaa83fd33add96
Tommy JOUHANS <12119@holbertonstudents.com>
2026-04-21 14:46:40 +0200

    12-log_stats.py

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -5,7 +5,7 @@ from pymongo import MongoClient
 
 if __name__ == "__main__":
     client = MongoClient('mongodb://127.0.0.1:27017')
-    nginx = client.logs.logs
+    nginx = client.logs.nginx
     print("{} logs".format(nginx.count_documents({})))
     print("Methods:")
     for method in ["GET", "POST", "PUT", "PATCH", "DELETE"]:
