aeb194bd72290ba081dad20259bc6c184742276d
Tommy JOUHANS <12119@holbertonstudents.com>
2026-04-22 21:41:09 +0200

    101-students.py

---
+++ NoSQL/101-students.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+""" Write a Python function that returns all
+students sorted by average score
+in descending order. """
+
+
+def top_students(mongo_collection):
+    """ mongo_collection will be the pymongo collection object
+        - The top must be ordered
+        - The average score must be part of each item
+          returns with key = averageScore
+    """
+    top_student = mongo_collection.aggregate([
+        {
+            "$project": {
+                "name": "$name",
+                "averageScore": {"$avg": "$topics.score"}
+            }
+        },
+        {"$sort": {"averageScore": -1}}
+    ])
+
+    return top_student
