05c5168feca87d836ed6c9914cf3aee93be2e55f
Tommy JOUHANS <12119@holbertonstudents.com>
2026-04-27 17:12:50 +0200

    6-string-interpolation.js

--- ES6_basic/6-string-interpolation.js
+++ ES6_basic/6-string-interpolation.js
@@ -4,10 +4,14 @@ export default function getSanFranciscoDescriptions() {
         income: '$119,868',
         gdp: '$154.2 billion',
         capita: '$178,479',
+        
     };
 
-    const description = `As of ${year}, it was the seventh-highest income county in the United States', with a per capita personal income of ${budget.income}.\n`
-      + `As of 2015, San Francisco proper had a GDP of ${budget.gdp}, and a GDP per capita of ${budget.capita}.`;
+    const str0 = 'As of ';
+    const str1 = ', it was the seventh-highest income county in the United States, with a per capita personal income of ';
+    const str2 = '.\n As of 2015, San Francisco proper had a GDP of ';
+    const str3 = ', and a GDP per capita of ';
+    const str4 = '.';
 
-      return description;
+return `${str0}${year}${str1}${budget.income}${str2}${budget.gdp}${str3}${budget.capita}${str4}`;
 }
