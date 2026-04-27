10c0cf2689e91614612b6b8da71a89525b4e4793
Tommy JOUHANS <12119@holbertonstudents.com>
2026-04-27 16:24:52 +0200

    12-createReportObject.js

--- ES6_basic/11-createEmployeesObject.js
+++ ES6_basic/11-createEmployeesObject.js
@@ -1,7 +1,3 @@
-// Write a function named createEmployeesObject that will receive two arguments:
-// - departmentName (string)
-// - employees (array of strings)
-// The function should return an object with the departmentName as a key and the value should be the array of employees.
 export default function createEmployeesObject(departmentName, employees) {
     const employee = {
         [departmentName]: employees,
