2f5d47d00d1d0f2beb893b34b10e7b9f803134ba
Tommy JOUHANS <12119@holbertonstudents.com>
2026-04-28 14:21:15 +0200

    5-building.js

---
+++ ES6_classes/5-building.js
@@ -0,0 +1,23 @@
+export default class Building {
+    constructor(sqft) {
+        if (typeof sqft !== 'number') {
+            throw new TypeError('Square footage must be a number');
+        }
+        this._sqft = sqft;
+    }
+
+    get sqft() {
+        return this._sqft;
+    }
+
+    set sqft(newSqft) {
+        if (typeof newSqft !== 'number') {
+            throw new TypeError('Square footage must be a number');
+        }
+        this._sqft = newSqft;
+    }
+    
+    evacuationWarningMessage()  {
+        return 'Class extending Building must override evacuationWarningMessage';
+    }  
+}
