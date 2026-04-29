f49deb5ce048515b49e5a16e96153ee3f8a70656
Tommy JOUHANS <12119@holbertonstudents.com>
2026-04-29 09:26:23 +0200

    100-evcar.js

---
+++ ES6_classes/100-evcar.js
@@ -0,0 +1,18 @@
+import Car from "./10-car.js";
+
+export default class EVCar extends Car {
+    constructor(brand, motor, color, range) {
+        super(brand, motor, color);
+        this._range = range;
+    }
+
+
+    static get [Symbol.species]() {
+    return this;
+    }
+
+    cloneCar() {
+        const Species = this.constructor[Symbol.species];
+        return new Species();
+    }
+}   
