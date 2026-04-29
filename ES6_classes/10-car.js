90b2cfd239420321ba5952e3ba455bdf040048d6
Tommy JOUHANS <12119@holbertonstudents.com>
2026-04-29 09:18:50 +0200

    10-car.js

--- ES6_classes/10-car.js
+++ ES6_classes/10-car.js
@@ -1,7 +1,7 @@
 export default class Car {
-  constructor(brand, model, color) {
+  constructor(brand, motor, color) {
     this._brand = brand;
-    this._model = model;
+    this._motor = motor;
     this._color = color;
   }
 
