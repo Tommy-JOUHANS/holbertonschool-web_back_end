5f2a0915298c785a2270d9287b81f8a2cf7761fd
Tommy JOUHANS <12119@holbertonstudents.com>
2026-05-04 10:37:00 +0200

    1-main.js

--- ES6_data_manipulation/1-get_list_student_ids.js
+++ ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,10 +1,7 @@
-export default function getListStudentsIds() {
-  const students = (
-    [
-    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
-    { id: 2, firstName: 'James', location: 'Columbia' },
-    { id: 5, firstName: 'Serena', location: 'San Francisco' }
-    ]
-  );
-  return students.map((student) => student.id);
+export default function getListStudentsIds(arr) {
+  
+  if (!Array.isArray(arr)) {
+    return [];
+  }
+  return arr.map((student) => student.id);
 }
