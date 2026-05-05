0fd7faf47ef823651085ac02789341cb7ba1a01a
Tommy JOUHANS <12119@holbertonstudents.com>
2026-05-05 10:33:50 +0200

    100-weak.js

---
+++ ES6_data_manipulation/100-weak.js
@@ -0,0 +1,13 @@
+const weakMap = new WeakMap();
+
+function queryAPI(endpoint) {
+    if (weakMap.has(endpoint) && weakMap.get(endpoint) >= 5) {
+        throw new Error('Endpoint load is high');
+    }
+    if (!weakMap.has(endpoint)) {
+        weakMap.set(endpoint, 0);
+    }
+    weakMap.set(endpoint, weakMap.get(endpoint) + 1);
+}
+
+export { queryAPI, weakMap };
