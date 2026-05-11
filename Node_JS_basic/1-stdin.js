51cc821a865592370e25ee788330cdc0d2a913eb
Tommy JOUHANS <12119@holbertonstudents.com>
2026-05-11 12:14:14 +0200

    1-stdin.js

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -2,8 +2,8 @@ console.log('Welcome to Holberton School, what is your name?');
 
 process.stdin.setEncoding('utf8');
 
-process.stdin.on('data', (INPUT) => {
-  const name = INPUT.trim();
+process.stdin.on('data', (chunk) => {
+  const name = chunk.trim();
   console.log('Your name is:', name);
   console.log('This important software is now closing');
   process.exit();
