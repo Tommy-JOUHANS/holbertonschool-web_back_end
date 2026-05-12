bb68b50a453932e1cb2dc1572559ef9fdebbc0dc
Tommy JOUHANS <12119@holbertonstudents.com>
2026-05-12 10:17:58 +0200

    4-http.js

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -5,6 +5,4 @@ const app = http.createServer((req, res) => {
   res.end('Hello Holberton School!');
 });
 
-app.listen(1245, () => {
-  console.log('Hello Holberton School!');
-});
+app.listen(1245);
